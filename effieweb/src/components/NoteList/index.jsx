1bb253381b80bb816f13f77fb7935fa05997e891
RoronuaHong <807672412@qq.com>
2022-07-13 19:07:00 +0800

    sidenavbar css completed

--- effieweb/src/components/NoteList/index.jsx
+++ effieweb/src/components/NoteList/index.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
 
+import "./index.scss"
+
 const NoteList = () => {
   return (
-    <div>i am NoteList</div>
+    <div className="note-list">
+      <div className="note-list-header">
+        <div className="note-list-header-title">
+          <h1>All Notes</h1>
+        </div>
+        <div className="note-list-header-sub-head">
+          <div className="note-count">
+            2 notes
+          </div>
+        </div>
+      </div>
+      <div className="note-list-body">
+        <div className="note-card">
+          <div className="note-card-head">
+            <div className="note-card-title">
+              Note One
+            </div>
+            <div className="note-card-desc">
+              Some Descrpition
+            </div>
+          </div>
+          <div className="note-card-date">
+            12 July 2022
+          </div>
+        </div>
+      </div>  
+    </div>
   )
 }
 
--- effieweb/src/components/SideNavBar/index.jsx
+++ effieweb/src/components/SideNavBar/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons'
+import { faAngleDown, faStickyNote, faSearch, faTrash, faPlus, faStar, faInfo } from '@fortawesome/free-solid-svg-icons'
 
 import './index.scss'
 
@@ -16,6 +16,42 @@ const SideNavbar = () => {
             SigmaHong
             <FontAwesomeIcon className="icon" icon={faAngleDown} />
           </div>
+        </div>
+        <div className="sidenavbar-top-search">
+          <div className="search-block">
+            <FontAwesomeIcon className="icon" icon={faSearch} />
+            <input type="text" placeholder="Search" />
+          </div>
+        </div>
+        <div className="sidenavbar-top-create-notes">
+          <div className="create-note-btn">
+            <FontAwesomeIcon className="icon" icon={faPlus} />
+            <div className="title">
+              New Note
+            </div>
+          </div>
+        </div>
+        <div className="sidenavbar-top-menu-item">
+          <ul>
+            <li>
+              <FontAwesomeIcon className="icon" icon={faStar} />
+              Sigma.
+            </li>
+            <li>
+              <FontAwesomeIcon className="icon" icon={faStickyNote} />
+              All Notes
+            </li>
+            <li>
+              <FontAwesomeIcon className="icon" icon={faTrash} />
+              Trash
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="sidenavbar-bottom">
+        <div className="sidenavbar-bottom-need-help">
+          <FontAwesomeIcon className="icon" icon={faInfo} />
+          Need a little help?
         </div>
       </div>
     </div>
